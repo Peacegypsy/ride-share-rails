988a57bde988b01c618dee7e729edb87b749ed58
Xtina <2006peacegypsy@gmail.com>
2018-10-02 15:25:54 -0700

    added show to passengers to view individual passenger information

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,7 +4,12 @@ class PassengersController < ApplicationController
   end
 
   def show
+    id = params[:id].to_i
+    @passenger = Passenger.find_by(id: id)
 
+    if @passenger.nil?
+      render :notfound, status: :not_found
+    end
   end
 
     def create
