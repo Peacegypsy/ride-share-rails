a199885212dfe356f815255e7da602543f2d04ec
Xtina <2006peacegypsy@gmail.com>
2018-10-02 11:45:02 -0700

    added new driver form,and it works

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,11 +1,19 @@
 class DriversController < ApplicationController
   def index
-    @drivers = Driver.all  
-    end
-
-    def create
+    @drivers = Driver.all
     end
 
     def new
+      @driver = Driver.new
     end
-end
+
+    def create
+      @driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+      if @driver.save
+        redirect_to drivers_path # go to the index so we can see the book in the list
+      else
+        render :new # show the new book form view again
+      end
+    end
+
+  end
