6f1507ced38bb676c92aa42df953b7f76176c839
Xtina <2006peacegypsy@gmail.com>
2018-10-04 12:01:37 -0700

    checking links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :drivers
-  resources :passengers
+
+    # /passengers/:passenger_id/trips/create
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
+
+
   resources :trips
+
+  patch '/drivers/:id/change_status', to: 'drivers#change_status', as: 'remove_driver'
+
+  patch '/passengers/:id/change_status', to: 'passengers#change_status', as: 'remove_passenger'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
