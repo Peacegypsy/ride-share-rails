3912ed6e6d18ed8f99ec0d96bb5a050c86703191
Jane <jane.edwards.mcnear@gmail.com>
2018-10-03 18:27:09 -0700

    Added add a new trip to passenger view page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,19 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def create
+    if params[:passenger_id]
+      driver =  Driver.find_by(status: "available")
+      if driver.nil?
+        render :drivernotfound, status: :not_found
+      else
+        @trip = Trip.new( driver_id: driver.id, passenger_id: params[:passenger_id], date: Date.today, cost: 0)
+        @trip.save
+        driver.update(status: "unavailable")
+        redirect_to passenger_path(params[:passenger_id])
+      end
+    end
+  end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
-  
+
   def currency
-     return "$#{'%.2f' % (self.cost * 0.01)}"
-   end
+    return "$#{'%.2f' % (self.cost * 0.01)}"
+  end
+
 end
