08e1f4ec4c602fc69f80ea847c01270963d7a6d9
Jane <jane.edwards.mcnear@gmail.com>
2018-10-04 19:15:38 -0700

    Changed it so it only updates the driver status if the deleted trip is active

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -54,7 +54,7 @@ class TripsController < ApplicationController
   def destroy
     id = params[:id]
     trip = Trip.find_by(id: id)
-    trip.driver.update(status: "available")
+    trip.driver.update(status: "available") if trip.rating.nil?
     if trip.destroy
       redirect_to passengers_path
     end
